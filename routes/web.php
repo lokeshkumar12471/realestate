8fbbe47b5322481b143d56b76dbb5aa6626f1415
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2023-08-02 00:54:26 +0530

    Admin Template Setup And Custom Login And Logout Created --01-08-2023

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,10 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
+Route::get('admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
