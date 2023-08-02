b915fd1ecce19e33392a66cec2969d06bacaf968
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2023-08-02 17:22:58 +0530

    Admin Profile Page Added --02-08-2023

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,5 +24,10 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect()->route('admin.login');
+    }
+    public function AdminProfile(){
+        $id=Auth::user()->id;
+        $profileData=User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
     }
 }
